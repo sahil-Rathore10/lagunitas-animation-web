66f461640ce089360da4ae61467c214836ff4897
Sahil Rathore <rathors378@gmail.com>
2025-07-31 00:03:51 +0530

    Final wrap up with gsap animation

--- script.js
+++ script.js
@@ -28,3 +28,63 @@ function show() {
 }
 
 show();
+
+gsap.to("#bottle", {
+  rotate: -15,
+  scrollTrigger: {
+    trigger: "#bottle",
+    scroller: "#main",
+    start: "top 5%",
+    end: "top -416%",
+    scrub: true,
+    pin: true,
+  },
+});
+
+gsap.to("#bottle", {
+  scale: "0.5",
+  scrollTrigger: {
+    trigger: "#page5 h1",
+    scroller: "#main",
+    start: "top 430%",
+    end: "top -430%",
+    scrub: true,
+    pin: true,
+  },
+});
+
+let t1 = gsap.timeline();
+t1.from("#main #page1_dog_image", {
+  opacity: 0,
+  duration: 1,
+  scale: 0.1,
+});
+
+t1.from("#bottle", {
+  opacity: 0,
+  duration: 1,
+  scale: 0.2,
+});
+
+t1.from("#nav_top>button", {
+  xPercent: 200,
+});
+
+gsap.from("page2_part1>button", {
+  scrollTrigger: {
+    trigger: "#page2_page1>button",
+    scroller: "#main",
+    start: "top 70%",
+  },
+  xPercent: -300,
+  duration: 1,
+});
+gsap.from("page6_part2>button", {
+  scrollTrigger: {
+    trigger: "#page6_part2>button",
+    scroller: "#main",
+    start: "top 70%",
+  },
+  xPercent: 600,
+  duration: 1,
+});
